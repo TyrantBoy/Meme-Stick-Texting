e8b715aa3589951525afaeb936a0585efb9e5cb0
Donald Nguyen <Zen@donalds-macbook-pro.local>
2016-08-19 14:10:27 -0500

    Chocolate Switch Filter

--- MessagesExtension/MessagesViewController.swift
+++ MessagesExtension/MessagesViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 import Messages
 
+
+protocol Chocoholicable {
+    func setChocoholic(_ chocoholic: Bool)
+}
+
 class MessagesViewController: MSMessagesAppViewController {
     
+    @IBAction func handleChocoholicChange(_ sender: UISwitch) {
+        childViewControllers.forEach({ vc in
+            guard let vc = vc as? Chocoholicable else { return }
+            vc.setChocoholic(sender.isOn)
+        })
+    }
   //testing
 }
