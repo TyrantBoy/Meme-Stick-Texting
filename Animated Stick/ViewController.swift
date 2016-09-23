852a4331c002f5f142d36f2cf722fbc4419789dc
TyrantBoy <Zen@donalds-macbook-pro.local>
2016-09-22 21:37:33 -0500

    updating files

---
+++ Animated Stick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animated Stick
+//
+//  Created by Donald Nguyen on 9/22/16.
+//  Copyright © 2016 Donald Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
